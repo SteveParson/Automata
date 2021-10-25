51d5908450dc3bd8be5d09029b9e694dcd0e33b1
Steve Parson <k47swp@mun.ca>
2021-10-25 19:37:08 -0300

    refactor: clearer logic

--- plugins/SentimentAnalysis/__init__.py
+++ plugins/SentimentAnalysis/__init__.py
@@ -16,17 +16,21 @@ class SentimentAnalysis(AutomataPlugin):
     async def sentiment(self, ctx, *, argument=None):
         """Replies with the sentiment of the sentence"""
 
-        message_to_reply_to = ctx.message
-        message_to_be_scored = argument
+        has_argument = False if argument is None else True
+        has_message_reference = False if ctx.message.reference is None else True
 
-        if argument is None and ctx.message.reference is None:
+        if not has_argument and not has_message_reference:
             historical_messages = await ctx.channel.history(limit=2).flatten()
             message_to_reply_to = historical_messages[1]
             message_to_be_scored = message_to_reply_to.content
 
-        elif argument is None and ctx.message.reference is not None:
+        elif not has_argument and has_message_reference:
             message_to_reply_to = await ctx.fetch_message(ctx.message.reference.message_id)
-            message_to_be_scored =  message_to_reply_to.content
+            message_to_be_scored = message_to_reply_to.content
+
+        else:
+            message_to_reply_to = ctx.message
+            message_to_be_scored = argument
 
         sentiment_text = ''
         output_template = "<@{author}>: This text is **{sentiment_text}**."
